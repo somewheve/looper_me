aa8d2e973a36f0f1f5fbcc9c104b64b1107c296d
somewheve <somewheve@gmail.com>
2019-08-11 17:55:03 +0800

    update readme

--- serve.py
+++ serve.py
@@ -1,4 +1,5 @@
 import os
+from configparser import ConfigParser
 
 from tornado.ioloop import IOLoop
 
@@ -7,16 +8,19 @@ from application.tcp_server import md_server, trade_server
 
 if __name__ == '__main__':
     try:
-        md_port = 12572
+        cfg = ConfigParser()
+        cfg.read('serve_config.ini')
+        md_port = cfg.getint('serve', 'md_port')
+        td_port = cfg.getint('serve', 'td_port')
+        http_port = cfg.getint('serve', 'http_port')
         md_server.listen(md_port)
         md_server.start()
-        td_port = 12472
         trade_server.listen(td_port)
         trade_server.start()
-        http_port = 8888
         app = make_app()
         app.listen(http_port)
-        os.system(f"""echo '数据服务器成功启动^_^ : \n   行情服务器地址---------> tcp://127.0.0.1:{md_port}\n   交易服务器地址---------> tcp://127.0.0.1:{td_port} \n   http服务器-----------> http://127.0.0.1:{http_port}
+        os.system(
+            f"""echo '数据服务器成功启动^_^ : \n   行情服务器地址---------> tcp://127.0.0.1:{md_port}\n   交易服务器地址---------> tcp://127.0.0.1:{td_port} \n   http服务器-----------> http://127.0.0.1:{http_port}
         '""")
 
     except Exception as e:
